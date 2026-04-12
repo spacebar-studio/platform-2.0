0ac4a8b7266991f99fe830dae0887d1c13c48253
Patel <patel@Patels-MacBook-Pro.local>
2026-04-11 23:57:18 -0500

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Platform2CaseStudy from '../mosey-platform-2-case-study.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Platform2CaseStudy />
+  </React.StrictMode>,
+)
